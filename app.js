67323c78b1c6c5fa5b13b2e1deff9e15be4dfc18
Rod1204 <rodrigovedeschi@gmail.com>
2021-12-06 22:32:19 -0300

    novo app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
-    res.writeHead(200, {"Content-type" : "text/playn" });
+// http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-type" : "text/playn" });
     
-        switch(req.url){
-            case "/":
-                res.end("Você está na página home!")
-                break;
-            case "/contato":
-                res.end("Você está na página contato!")
-                break
-            default:
-                res.end("Você está no nosso servidor");
-                break
-        }
-}).listen(3000);
+//         switch(req.url){
+//             case "/":
+//                 res.end("Você está na página home!")
+//                 break;
+//             case "/contato":
+//                 res.end("Você está na página contato!")
+//                 break
+//             default:
+//                 res.end("Você está no nosso servidor");
+//                 break
+//         }
+// }).listen(3000);
+
+const express = require('express');
+
+let app = express();
+
+console.log(app);
